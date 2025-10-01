0bd51f802be599e49c0e343bbaed0371457daade
vice.parada <vice.parada@duocuc.cl>
2025-10-01 23:13:41 +0000

    primer commit

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ const app = express();
 const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
 
 app.use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/api', (req, res) => {
