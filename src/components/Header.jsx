4670799e4ec94e03b38d2abefa23d8b3dade2945
vice.parada <vice.parada@duocuc.cl>
2025-10-07 22:45:26 +0000

    correcciones extra

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header() {
   return (
     <header id="header" className="fixed-top header-transparent">
-      <div className="container d-flex align-items-center justify-content-between">
+      <div className="container d-flex align-items-center">
         {/* Logo */}
         <div className="logo">
           <h1 className="text-light m-0">
